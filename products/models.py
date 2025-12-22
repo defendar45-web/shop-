9ad6dc7a9c788857a36b8a25d08b83dc407e5891
isHardCoded <ishardcoded@gmail.com>
2025-12-22 17:05:27 +0300

    Сделали slug для Product

--- products/models.py
+++ products/models.py
@@ -15,17 +15,12 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField()
-    slug = models.SlugField(max_length=100, unique=True , null=True,blank=True)
+    slug = models.SlugField(null=False, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="products/", null=True, blank=True)
 
     def get_image(self):
         return self.image.url
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
